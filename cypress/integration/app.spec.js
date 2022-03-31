ba5fd68a45b75cde9b2f031b463c3f483b61de52
fargumedo <48562550+fargumedo@users.noreply.github.com>
2022-03-31 15:07:01 -0600

    Testing learn 1

---
+++ cypress/integration/app.spec.js
@@ -0,0 +1,41 @@
+describe("React TodoMVC", () => {
+    const TODO_ITEM_ONE = "Buy Milk"
+    const TODO_ITEM_TWO = "Pay Rent"
+    const TODO_ITEM_THREE = "Pickup Dry Cleaning"
+
+    beforeEach(() => {
+        cy.visit("http://localhost:8888")
+    })
+
+    it("adds a single todo", () => {        
+        cy.get(".new-todo").type("Buy Milk{enter}")
+        cy.get(".todo-list li").should("have.length", 1)
+    })
+
+    it("adds three todos", () => {
+        cy.get(".new-todo").type(`${TODO_ITEM_ONE}{enter}`)
+        cy.get(".new-todo").type(`${TODO_ITEM_TWO}{enter}`)
+        cy.get(".new-todo").type(`${TODO_ITEM_THREE}{enter}`)
+        cy.get(".todo-list li").should("have.length", 3)
+        cy.get(".todo-list li").eq(0).find("label").should("contain", TODO_ITEM_ONE)
+        cy.get(".todo-list li").eq(1).find("label").should("contain", TODO_ITEM_TWO)
+        cy.get(".todo-list li").eq(2).find("label").should("contain", TODO_ITEM_THREE)
+    })
+
+    it("should append new items to the bottom of the list", () => {
+        cy.createDefaultTodos()
+        cy.get(".todo-list li").should("have.length", 3)
+
+        //Todo 1
+        cy.get(".todo-list li").eq(0).find("label").should("contain", TODO_ITEM_ONE)
+
+        //Todo2
+        cy.get(".todo-list li").eq(1).find("label").should("contain", TODO_ITEM_TWO)
+
+        //Todo3
+        cy.get(".todo-list li").eq(2).find("label").should("contain", TODO_ITEM_THREE)
+
+        cy.get(".todo-count").contains("3 items left")
+    })
+
+})
